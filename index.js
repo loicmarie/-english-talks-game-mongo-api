f0b786186a5a312b47a1500234b2887a83c7e291
Wordam <niamor123@gmail.com>
2016-09-26 19:53:19 +0200

    allow cross-domain request

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ let server = restify.createServer();
 /* bodyParser allows to parse body parameters when handling requests */
 server.use(restify.bodyParser({ mapParams: true }));
 
+
+server.use(restify.CORS({
+
+  // Defaults to ['*'].
+  origins: ['http://localhost:8080'],
+
+  // Defaults to false.
+  credentials: true,
+
+  // Sets expose-headers.
+  headers: ['x-foo']
+
+}));
+
+
 require('./routes/index')(server);
 
-server.listen(8080, function() {
+server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
